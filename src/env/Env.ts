0e3d683d1f33cd055b76b520ac5c02e7cf4adad0
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2025-04-11 22:31:59 -0300

    Fixed

--- src/env/Env.ts
+++ src/env/Env.ts
@@ -6,6 +6,8 @@ config();
 const envSchema = z.object({
     ENV: z.string(),
     APP_PORT: z.coerce.number(),
+    GMAIL_USER: z.string(),
+    GMAIL_PASS: z.string(),
 })
 
 const Env = envSchema.parse(process.env);
